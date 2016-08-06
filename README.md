61871e99c8ad76e1d0a37bb677e8181378b847b5
pengshiqi <617225573@qq.com>
2016-08-06 22:06:56 +0800

    add html and js

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # OpticalCharacterRecognition
-这是一个手写字符识别系统。
+
+本项目基于BP神经网络实现一个手写字符识别系统，系统会在服务器启动时自动读入训练好的神经网络文件，如果文件不存在，则会读入数据集开始训练，用户可以通过在html页面上手写数字发送给服务器来得到识别结果。
+
+### 系统组成
+
+* 客户端(*ocr.js*)
+
+* 服务器(*server.py*)
+
+* 用户接口(*ocr.html*)
+
+* 神经网络(*ocr.py*)
+
+* 神经网络设计脚本(*neural_network_design.py*)
+
+用户接口(ocr.html)是一个html页面，用户在canvas上写数字，之后点击选择训练或是预测。客户端(ocr.js)将收集到的手写数字组合成一个数组发送给服务器端(server.py)处理，服务器调用神经网络模块(ocr.py)，它会在初始化时通过已有的数据集训练一个神经网络，神经网络的信息会被保存在文件中，等之后再一次启动时使用。最后，神经网络设计脚本(neural_network_design.py)是用来测试不同隐藏节点数下的性能，决定隐藏节点数用的。
+
+## Reference
+
+* [实验楼](https://www.shiyanlou.com/courses/593/labs/1966/document)
+
+* [500 lines or less](https://github.com/aosabook/500lines/tree/master/ocr)
---
+++ ocr.js
@@ -0,0 +1,62 @@
+var ocrDemo = {
+  CANVAS_WIDTH: 200,
+  TRANSLATE_WIDTH: 20,
+  PIXEL_WIDTH: 10, // TRANSLATE_WIDTH = CANVAS_WIDTH / PIXEL_WIDTH
+  BATCH_SIZE: 1,
+
+  // Server params
+  PORT: '9000',
+  HOST: 'http://localhost',
+
+  // Color var
+  BLACK: '#000000',
+  BLUE: '#0000FF',
+
+  // Server training set
+  trainArray: [],
+  trainingRequestCount: 0,
+
+  onLoadFunction: function() {
+    this.resetCanvas();
+  },
+
+  resetCanvas: function() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    this.data = [];
+    ctx.fillStyle = this.BLACK;
+    ctx.fillRect(0, 0, this.CANVAS_WIDTH, this.CANVAS_WIDTH);
+    var matrixSize = 400;
+    while (matrixSize--)
+      this.data.push(0);
+    this.drawGrid(ctx);
+
+    // bind event operation
+    canvas.onmousemove = function(e) {this.onMouseMove(e, ctx, canvas)}.bind(this);
+    canvas.onmousedown = function(e) {this.onMouseDown(e, ctx, canvas)}.bind(this);
+    canvas.onmouseup = function(e) {this.onMouseUp(e, ctx)}.bind(this);
+  },
+
+  drawGrid: function(ctx) {
+    for (var x = this.PIXEL_WIDTH, y = this.PIXEL_WIDTH; x < this.CANVAS_WIDTH; x += this.PIXEL_WIDTH, y += this.PIXEL_WIDTH) {
+      ctx.strokeStyle = this.BLUE;
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, this.CANVAS_WIDTH);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(y, 0);
+      ctx.lineTo(this.CANVAS_WIDTH, y);
+      ctx.stroke();
+    }
+  },
+
+  onMouseMove: function(e, ctx, canvas) {
+    if (!canvas.isDrawing) {
+      
+    }
+  },
+
+}
