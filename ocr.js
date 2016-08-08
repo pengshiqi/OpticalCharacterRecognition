2230b3dc50eeedac3ab0d883b5318532b414fa74
pengshiqi <617225573@qq.com>
2016-08-08 22:56:14 +0800

    add server

--- ocr.js
+++ ocr.js
@@ -33,9 +33,9 @@ var ocrDemo = {
     this.drawGrid(ctx);
 
     // bind event operation
-    canvas.onmousemove = function(e) {this.onMouseMove(e, ctx, canvas)}.bind(this);
-    canvas.onmousedown = function(e) {this.onMouseDown(e, ctx, canvas)}.bind(this);
-    canvas.onmouseup = function(e) {this.onMouseUp(e, ctx)}.bind(this);
+    canvas.onmousemove = function(e) { this.onMouseMove(e, ctx, canvas) }.bind(this);
+    canvas.onmousedown = function(e) { this.onMouseDown(e, ctx, canvas) }.bind(this);
+    canvas.onmouseup = function(e) { this.onMouseUp(e, ctx) }.bind(this);
   },
 
   drawGrid: function(ctx) {
@@ -47,7 +47,7 @@ var ocrDemo = {
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.moveTo(y, 0);
+      ctx.moveTo(0, y);
       ctx.lineTo(this.CANVAS_WIDTH, y);
       ctx.stroke();
     }
@@ -57,17 +57,88 @@ var ocrDemo = {
     if (!canvas.isDrawing) {
       return;
     }
-    this.fillSquare(ctx, e.clientX - canvas.offSetLeft, e.clientY - canvas.offSetTop)
+    this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+  },
+
+  onMouseDown: function(e, ctx, canvas) {
+    canvas.isDrawing = true;
+    this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
   },
 
   onMouseUp: function(e) {
     canvas.isDrawing = false;
   },
 
+  fillSquare: function(ctx, x, y) {
+    var xPixel = Math.floor(x / this.PIXEL_WIDTH);
+    var yPixel = Math.floor(y / this.PIXEL_WIDTH);
+    // 存储手写输入数据
+    this.data[((xPixel - 1) * this.TRANSLATE_WIDTH + yPixel) - 1] = 1;
 
-  onMouseDown: function(e, ctx, canvas) {
-    canvas.isDrawing = true;
-    this.fillSquare(ctx, e.clientX - canvas.offSetLeft, e.clientY - canvas.offSetTop)
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(xPixel * this.PIXEL_WIDTH, yPixel * this.PIXEL_WIDTH, this.PIXEL_WIDTH, this.PIXEL_WIDTH);
+  },
+
+  train: function() {
+    var digitVal = document.getElementById('digit').value;
+    if (!digitVal || this.data.indexOf(1) < 0) {
+      alert('Please type and draw a digit value in order to train the network');
+      return;
+    }
+    // 将数据加入客户端训练数据集
+    this.trainArray.push({'y0': this.data, 'label': parseInt(digitVal)});
+    this.trainingRequestCount += 1;
+    // 将客户端训练数据集发送给服务器端
+    if (this.trainingRequestCount == this.BATCH_SIZE) {
+      alert('Sending training data to server...');
+      var json = {
+        trainArray: this.trainArray,
+        train: true
+      };
+
+      this.sendData(json);
+      this.trainingRequestCount = 0;
+      this.trainArray = [];
+    }
+  },
+
+  // 发送预测请求
+  test: function() {
+    if (this.data.indexOf(1) < 0) {
+      alert('Please draw a digit in order to test the network');
+      return;
+    }
+    var json = {
+      image: this.data,
+      predict: true
+    }
+    this.sendData(json);
+  },
+
+  // 处理服务器响应
+  receiveResponse: function(xmlHttp) {
+    if (xmlHttp.status != 200) {
+      alert('Server returned status ' + xmlHttp.status);
+      return;
+    }
+    var responseJSON = JSON.parse(xmlHttp.responseText);
+    if (xmlHttp.responseText && responseJSON.type == 'test') {
+      alert('The neural network predicts you wrote a \'' + responseJSON.result + '\'');
+    }
+  },
+
+  onError: function(e) {
+    alert('Error occured while connecting to server: ' + e.target.statusText);
+  },
+
+  sendData: function(json) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('POST', this.HOST + ':' + this.port, false);
+    xmlHttp.onload = function() { this.receiveResponse(xmlHttp); }.bind(this);
+    xmlHttp.onerror = function() { this.onError(xmlHttp); }.bind(this);
+    var msg = JSON.stringify(json);
+    xmlHttp.setRequestHeader('Content-length', msg.length);
+    xmlHttp.setRequestHeader('Connection', 'close');
+    xmlHttp.send(msg);
   }
-
 }
